b57c152a4a9844bee91ec0132dd9e6d0df7a262e
Elli Raynai <elli@elliraynai.com>
2021-07-29 18:13:04 -0400

    first project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+// Add the Route named import
+import { Route, Switch, Redirect } from 'react-router-dom';
+import AuthPage from './pages/AuthPage/AuthPage';
+
+class App extends Component {
+	state = {
+		user: null,
+	};
+	setUserInState = (incomingUserData) => {
+		this.setState({ user: incomingUserData });
+	};
+	// when the page refreshes, check localStorage for the user jwt token
+	componentDidMount() {
+		let token = localStorage.getItem('token');
+		if (token) {
+			// YOU DO: check expiry!
+			let userDoc = JSON.parse(atob(token.split('.')[1])).user; // decode jwt token
+			this.setState({ user: userDoc });
+		}
+	}
+
+	render() {
+		return (
+			<main className="App">
+				<AuthPage setUserInState={this.setUserInState} />
+			</main>
+		);
+	}
+}
+
+export default App;
