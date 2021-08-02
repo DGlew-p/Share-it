96d2d5f7b1e560dc0695b4940e5e03d336b2a093
Elli Raynai <elli@elliraynai.com>
2021-08-02 12:43:00 -0400

    navigation working

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,10 +1,12 @@
 
 import { Component } from 'react'
 
+
 export default class Projects extends Component {
     render(){
         return(
             <div>
+                
                 <h1>Project Page!</h1>
             </div>
         )
