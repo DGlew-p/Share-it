6111f3bfa1eb6ecb9fea94d7c096abadf5409ecf
Elli Raynai <elli@elliraynai.com>
2021-08-03 11:51:04 -0400

    fixed issues

--- controllers/projects.js
+++ controllers/projects.js
@@ -18,70 +18,17 @@ function index(req, res) {
 
 // };
 
-<<<<<<< HEAD
 async function create(req, res) {
 	try {
-		const Project = await Project.create({
-			// objectIdReference : req.body.user_id,
-			title: req.body.title,
-			image_upload: req.body[(url, filename)],
-			tech_stack: req.body.tech_stack,
-			project_description: req.body.project_description,
-		});
-		console.log(req.body);
-		// await newProject.save();
-		res.status(200).json();
+		let thing = await Project.create(req.body);
+		console.log(thing + '   THING');
+		// project.image_upload.url= req.file.path;
+		// project.image_upload.filename = req.file.filename;
+		res.status(200).json('CREATE');
 	} catch (err) {
-		return res.status(400).json('Bad Request');
+		return res.status(400).json('Bad control @ CREATE');
 	}
 }
-
-async function update(req, res) {
-	try {
-		const updatedProject = await Project.findById(req.params.id);
-
-		updatedProject.title = req.body.title;
-		updatedProject.image.url = req.file.path;
-		updatedProject.image.imageName = req.file.filename;
-		updatedProject.tech_stack = req.body.tech_stack;
-		updatedProject.project_description = req.body.project_description;
-
-		await updatedProject.save();
-		res.status(200).json('OK!');
-	} catch (err) {
-		return res.status(400).json('Bad Request');
-	}
-}
-
-async function deleteProject(req, res) {
-	await Project.findByIdAndDelete(req.body.id, function (err) {
-		if (err) return res.status(400).json(err);
-		res.status(200).json('deleted');
-	});
-}
-
-module.exports = {
-	index,
-	create,
-	update,
-	delete: deleteProject,
-	// projectDetail,
-};
-=======
-async function create (req, res){
-    try {
-       let thing = await Project.create(req.body)
-       console.log(thing+"   THING")
-          // project.image_upload.url= req.file.path;
-          // project.image_upload.filename = req.file.filename;
-          res.status(200).json('CREATE');
-      }
-      catch(err) {
-        return res.status(400).json('Bad control @ CREATE')
-    }
- 
-  };
-  
 
 // async function update (req, res){
 //     try {
@@ -108,11 +55,9 @@ async function create (req, res){
 //   }
 
 module.exports = {
-    index,
-    create,
-    // update,
-    // delete: deleteProject,
-    // projectDetail,
-
-} 
->>>>>>> e193d9b36ff42177b18e807e6d56e7b8fa14db84
+	index,
+	create,
+	// update,
+	// delete: deleteProject,
+	// projectDetail,
+};
